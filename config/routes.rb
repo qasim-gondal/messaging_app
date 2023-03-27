bf8e43e4d94ae56795d160329533d8612f26ed2c
qasim-gondal <qgondal48@gmail.com>
2023-03-27 23:18:28 +0500

    initial  commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
